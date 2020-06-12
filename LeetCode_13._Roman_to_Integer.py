2dbf50e2ab806fc499f5f8b5e6ef33997b390832
Huang-Min-Xiong <qwe31914@gmail.com>
2020-06-12 23:03:57 +0800

    LeetCode_13._Roman_to_Integer

---
+++ LeetCode_13._Roman_to_Integer.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+        dic = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        ans = 0
+        for i in range(len(s)):
+            if i+1<len(s) and dic[s[i+1]]>dic[s[i]]: 
+                ans -= dic[s[i]] #後位比前位大，用減法
+            else:
+                ans += dic[s[i]]  
+        return ans
+
+#Example
+s=Solution()
+s.romanToInt("LVIII")    
